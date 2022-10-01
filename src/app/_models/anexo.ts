f557d5499dec3c2c15aa9df4f1365183a8f79ce7
yiyi153 <yiyi153@gmail.com>
2022-10-01 11:22:56 -0500

    cambiomodelo
    se cambio el modelo anexo

--- src/app/_models/anexo.ts
+++ src/app/_models/anexo.ts
@@ -1,7 +1,20 @@
-﻿export interface Anexo {
-  id:number;
-  archivo: string;
-  fecha: Date;
-  hora: TimeRanges;
-  falta: any;
+﻿
+export class Anexo {
+  static AnexoDesdeJson(obj: object) {
+    return new Anexo(
+      obj["id"],
+      obj["archivo"],
+      obj["fecha"],
+      obj["hora"],
+      obj["falta"]
+    );
+  }
+
+  constructor(
+    public id: number,
+    public archivo: string,
+    public fecha: Date,
+    public hora: TimeRanges,
+    public falta: any
+  ) {}
 }
