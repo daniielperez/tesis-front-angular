a1c426b8338221739cfbeb45676c7fb3b6de4811
yiyi153 <yiyi153@gmail.com>
2022-10-03 21:05:57 -0500

    cambio modelo planestudios

--- src/app/_models/horario.ts
+++ src/app/_models/horario.ts
@@ -1,10 +1,26 @@
-﻿export interface Horario {
-  id:number;
-  dia: Date;
-  hinicio: TimeRanges;
-  tclase: string;
-  urlremota: string;
-  hfin: TimeRanges;
-  Idcarga: any;
-  Idsalon: any;
+﻿
+export class Horario {
+  static HorarioDesdeJson(obj: object) {
+    return new Horario(
+      obj["id"],
+      obj["dia"],
+      obj["hinicio"],
+      obj["tclase"],
+      obj["urlremota"],
+      obj["hfin"],
+      obj["Idcarga"],
+      obj["Idsalon"]
+    );
+  }
+
+  constructor(
+    public id: Number,
+    public dia: Date,
+    public hinicio: TimeRanges,
+    public tclase: string,
+    public urlremota: string,
+    public hfin: TimeRanges,
+    public Idcarga: Number,
+    public Idsalon: Number
+  ) {}
 }
---
+++ src/app/_models/planestudios.ts
@@ -0,0 +1,14 @@
+﻿
+export class Planestudios {
+  static PlanestudiosesdeJson(obj: object) {
+    return new Planestudios(
+      obj["id"],
+      obj["nombre"]
+    );
+  }
+
+  constructor(
+    public id: number,
+    public nombre: string
+  ) {}
+}
